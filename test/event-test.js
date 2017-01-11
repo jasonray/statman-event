b52a5d7b47956304e547248780ddd538cca5f3a3
Jason Ray <jasonray@jayray.net>
2017-01-10 19:18:17 -0500

    trimmed sample test

---
+++ test/event-test.js
@@ -0,0 +1,19 @@
+/*jslint node: true */
+"use strict";
+
+var Stopwatch = require('../lib/Stopwatch');
+var defaultPrecision = 15;
+var assert = require('assert');
+var should = require('should');
+
+describe('event', function () {
+    // this.timeout(5000);
+
+    describe('constructor', function () {
+        it('w/no params', function () {
+            // var stopwatch = new Stopwatch();
+            // should.exist(stopwatch);
+        });
+
+    });
+});
